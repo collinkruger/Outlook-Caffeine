1a4b5563f7dae041f5b211fd592374017b499362
Collin Kruger <collin.kruger@solve100.com>
2017-02-10 17:19:46 -0600

    V1

---
+++ OutlookCaffeine/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using Topshelf;
+
+namespace OutlookCaffeine
+{
+    public class Core
+    {
+        readonly Timer _timer;
+
+        void _timer_Elapsed(object _, ElapsedEventArgs __)
+        {
+            if (Process.GetProcessesByName("Outlook").Length == 0)
+                Process.Start("Outlook");
+        }
+
+        public Core()
+        {
+            _timer = new Timer();
+            _timer.Interval = 60 * 1000;
+            _timer.AutoReset = true;
+            _timer.Elapsed += _timer_Elapsed;
+        }
+
+        public void Start()
+        {
+            _timer.Start();
+        }
+
+        public void Stop()
+        {
+            _timer.Dispose();
+        }
+
+    }
+
+    public static class Program
+    {
+        public static void Main()
+        {
+            HostFactory.Run(x =>
+            {
+                x.Service<Core>(s =>
+                {
+                    s.ConstructUsing(() => new Core());
+                    s.WhenStarted(c => c.Start());
+                    s.WhenStopped(c => c.Stop());
+                });
+                x.RunAsPrompt();
+
+                x.SetDescription("Outlook Caffeine");
+                x.SetDisplayName("Outlook Caffeine");
+                x.SetDisplayName("Outlook Caffeine");
+                x.StartAutomatically();
+            });
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# Outlook-Caffeine
-Reopens Outlook When I Close It By Accident
+# Outlook Caffeine
+Reopens Outlook When You Close It By Accident
+
+---
+
+To install, move ```/Release/OutlookCaffeine.exe``` to whereever you want to keep the application permanently Ex.```"C:\Program Files (x86)\CK\OutlookCaffeine.exe"```
+
+The open an admin command prompt where you install the app and run the following. During installation you'll be prompted for your credentials (since Outlook is going to be opened "as you").
+
+```
+OutlookCaffeine.exe install
+```
+
+Then run the following to start the service for the first time (it's automatic when you restart your computer).
+
+```
+OutlookCaffeine.exe run
+```
